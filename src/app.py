c490cd0cea6897a66f23c6c47d25811a123abb66
CarlosCMbck <backend1@commonmarket.es>
2021-05-21 03:25:11 +0200

    check

--- src/app.py
+++ src/app.py
@@ -7,9 +7,11 @@ try:
     import time
     import json
     import requests
+    import speech_recognition as sr
     from dotenv import load_dotenv
-except:
+except Exception:
     print("Error: library not found")
+    print(Exception)
 
 
 #Inicializador de reconocimiento de voz
@@ -22,6 +24,18 @@ def hablar(texto):
     engine.say(texto)
     engine.runAndWait()
 
+
+def escuchar():
+    esc = sr.Recognizer() 
+    with sr.Microphone() as source:
+        raw = esc.listen(source)
+        try:
+            ret = esc.recognize_google(raw, languaje='es')
+            return {"ok":0, "return":ret}
+        except Exception:
+            return {"ok":1, "error": Exception}
+    sr.listen()
+        
 ##
 # Main function
 # 
@@ -32,31 +46,31 @@ try:
     while (exit==False):
         print("itereacion")
         hablar("Sistema online. Introduzca comando: ")
-        statement = ''
+        statement = escuchar()
         #Error
-        if statement.count==0:
-            print('Error')
+        if statement.ok==1:
+            print("Error: "+statement.error)
         #Salir
-        elif "parar programa" in statement:
+        elif "parar programa" in statement.ret:
             hablar('Sistema offline. Adios')
             exit = True
         
         #
-        elif 'abrir youtube' in statement:
+        elif 'abrir youtube' in statement.ret:
             webbrowser.open_new_tab("https://www.youtube.com")
             time.sleep(5)
         #
-        elif 'abrir google' in statement:
+        elif 'abrir google' in statement.ret:
             webbrowser.open_new_tab("https://www.google.com")
             time.sleep(5)
             
         #
-        elif 'abrir gmail' in statement:
+        elif 'abrir gmail' in statement.ret:
             webbrowser.open_new_tab("gmail.com")
             time.sleep(5)
             
         #
-        elif "apagar ordenador" in statement:
+        elif "apagar ordenador" in statement.ret:
             hablar("Apagando Ordenador")
             subprocess.call(["shutdown", "/l"])
 except:
