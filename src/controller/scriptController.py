418010fe60d8fb58600a15e2ba9957b2b4ed855d
CarlosCMbck <backend1@commonmarket.es>
2021-06-09 04:02:03 +0200

    great ominous deed

--- src/controller/scriptController.py
+++ src/controller/scriptController.py
@@ -29,33 +29,10 @@ def abrir():
 def guardar(script, nombre, accion, objeto):
     try:
         dicc = json.loads(diccionario)
-        for script in dicc.script:
-            if(script.accion == accion and script.objeto == objeto):
-                
-
         route = os.path.join(os.path.realpath('__file__'), ruta)
         route = os.path.abspath(os.path.realpath(route))
         fichero = open(route, 'w+')
         fichero.write(script)
         fichero.close()
-        mensaje.set("Fichero guardado correctamente")
     except:
-
-
-def guardar_como():
-    global ruta
-    mensaje.set("Guardar fichero como")
-    fichero = FileDialog.asksaveasfile(title="Guardar fichero",
-        mode="w", defaultextension=".py")
-    if fichero is not None:
-        ruta = fichero.name
-        print(ruta)
-        contenido = texto.get(1.0,'end-1c')
-        fichero = open(ruta, 'w+')
-        fichero.write(contenido)
-        fichero.close()
-        mensaje.set("Fichero guardado correctamente")
-        root.quit
-    else:
-        mensaje.set("Guardado cancelado")
-
+        print("error")
